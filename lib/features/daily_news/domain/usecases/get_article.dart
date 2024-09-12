2fa516acd285159e3579981b6366c636cc643942
AzureAD\FrankZhangMiningSkil <frank.zhang.sa.au@gmail.com>
2024-09-12 13:43:56 +1000

    1.  implements UseCase 2. in call method: use repo

--- lib/features/daily_news/domain/usecases/get_article.dart
+++ lib/features/daily_news/domain/usecases/get_article.dart
@@ -3,8 +3,10 @@ import 'package:news_app_clean_architecture/core/usecase/usecase.dart';
 import 'package:news_app_clean_architecture/features/daily_news/domain/entities/article.dart';
 import 'package:news_app_clean_architecture/features/daily_news/domain/repository/article_repository.dart';
 
+/// 01: class GetArticleUseCase implements UseCase
 class GetArticleUseCase implements UseCase<DataState<List<ArticleEntity>>,void>{
-  
+
+  /// 02: GetArticle UseCase needs a Repo
   final ArticleRepository _articleRepository;
 
   GetArticleUseCase(this._articleRepository);
